88a4b2208a7c5f932917e0687d28eb5289229c5a
vakaheydev <ivan.novgor257@gmail.com>
2026-01-06 14:41:50 +0300

    Security fix

--- rest-api/src/main/java/com/vaka/daily/controller/MeController.java
+++ rest-api/src/main/java/com/vaka/daily/controller/MeController.java
@@ -6,6 +6,7 @@ import com.vaka.daily.domain.User;
 import com.vaka.daily.service.domain.MeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,16 +23,19 @@ public class MeController {
         this.meService = meService;
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<User> getMe() {
         return ResponseEntity.ok(meService.getMe());
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/schedules")
     public ResponseEntity<List<Schedule>> getMySchedules() {
         return ResponseEntity.ok(meService.getMySchedules());
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> getMyTasks() {
         return ResponseEntity.ok(meService.getMyTasks());
